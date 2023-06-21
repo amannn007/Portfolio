24955709fbd4b827ceac97364857a08e908959b9
Aman Kumar <95896562+amannn007@users.noreply.github.com>
2023-06-21 21:43:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const fun=()=>{
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form ',{ origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .certification-container, .portfolio-box, .contact form ',{ origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
