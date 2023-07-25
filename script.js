bb8845a464eae5111b478a502bf67f679ce245a5
Aman Kumar <95896562+amannn007@users.noreply.github.com>
2023-07-25 13:16:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*############### typed js ############*/
 const typed= new Typed ('.multiple-text', {
-    strings: ['Frontend Developer','Web Developer','Sportsman'],
+    strings: ['MERN Developer','Full Stack Web Developer','Sportsman'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
